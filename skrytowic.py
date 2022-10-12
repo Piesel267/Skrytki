4940764a67cc709555c9729fe83007ac44d243a2
Xeno <77623828+Piesel267@users.noreply.github.com>
2022-10-12 19:07:07 +0200

    Update 1 🎉🎉
    Nowe:
    -Admin panel

---
+++ skrytowic.py
@@ -0,0 +1,25 @@
+from fileinput import close
+
+
+id2 = input("Wpisz login: ")
+id = input("Wpisz haslo: ")
+print( "\n" "Sprawdzam dane ...")
+print( "\n" "Sprawdzam dane ..")
+print( "\n" "Sprawdzam dane ...")
+
+print ("\n""Witaj,", id2,  "Zalogowano do admin panel🔥!")
+
+if id.lower() == "0926":
+    print("ID sprawdzone permisje nadane!")
+else:
+    print("Zle ID")
+
+if id.lower() == "0926":
+    ps1 = input("\n""Wpisz numer 1: ")
+    ps2 = input("Wpisz numer 2: ")
+else:
+    print("Invalid input")
+
+print("", )
+
+
